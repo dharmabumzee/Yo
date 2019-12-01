86c84b6b9adaf32650a1e11690a67a543abecbf9
dharmabumzee <luka.belani@gmail.com>
2019-12-01 21:33:13 +0100

    refactor Apod.js to stateless functional component

---
+++ src/components/Apod.js
@@ -0,0 +1,24 @@
+import React from "react";
+import MediaType from "./MediaType";
+import Spinner from "./Spinner";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import TinyButton from "./TinyButton";
+import { containerStyles } from "../styles.js";
+
+const Apod = ({ date, title, copyright, url, mediaType }) => {
+  return (
+    <Jumbotron className="apod">
+      <Container className="apod-container" style={containerStyles}>
+        <div className="date">{date}</div>
+        <div id="desc" className="desc">
+          {title} ➜ Copyright© {copyright}
+        </div>
+        {url ? <MediaType mediaType={mediaType} url={url} /> : <Spinner />}
+        <TinyButton />
+      </Container>
+    </Jumbotron>
+  );
+};
+
+export default Apod;
