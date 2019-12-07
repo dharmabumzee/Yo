70f1ace2a5b1870b1622661a6b8bd6991033ff02
dharmabumzee <luka.belani@gmail.com>
2019-12-07 17:53:26 +0100

    refactor to smaller components

--- src/components/MarsRoverPhotos.js
+++ src/components/MarsRoverPhotos.js
@@ -7,8 +7,12 @@ import imageUrl from "../background.png";
 import TinyButton from "./TinyButton";
 import HeaderLogo from "./HeaderLogo";
 import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
 import Menu from "./Menu";
+import RoverDropdown from "./RoverDropdown";
+import CuriosityRover from "./CuriosityRover";
+import OpportunityRover from "./OpportunityRover";
+import SpiritRover from "./SpiritRover";
+import EnterSol from './EnterSol';
 import {
   marsRoverStyles,
   roverDropdownStyles,
@@ -41,6 +45,7 @@ function MarsRoverPhotos() {
   
   const classes = useStyles();
 
+  
   // mars photos
   const marsSearch = () => {
     setLoading(true);
@@ -91,166 +96,18 @@ function MarsRoverPhotos() {
                       marsSearch();
                     }}
                   >
-                    <div>
-                      <select
-                        className="roverDropdown"
-                        value={rover}
-                        onChange={event => {
-                          event.preventDefault();
-                          setRover(event.target.value);
-                          setMaxSol("");
-                          setCamera("");
-
-                        }}
-                        style={roverDropdownStyles}
-                      >
-                        <option hidden>CHOOSE A ROVER</option>
-                        <option value="curiosity">
-                          CURIOSITY - ACTIVE MISSION
-                        </option>
-                        <option value="opportunity">
-                          OPPORTUNITY OPPY - RETIRED
-                        </option>
-                        <option value="spirit">
-                          SPIRIT ROVER MER-2 - RETIRED
-                        </option>
-                      </select>
-                    </div>
-
-                    {rover === "curiosity" && (
-                      <div>
-                        <select
-                          className="cameraDropdown"
-                          id="curiosityCameras"
-                          value={camera}
-                          onChange={event => {
-                            event.preventDefault();
-                            setCamera(event.target.value);
-                            findMaxSol();
-                          }}
-                          style={cameraDropdownStyles}
-                        >
-                          <option>SELECT A CAMERA</option>
-                          <option value="FHAZ">
-                            FRONT HAZARD AVOIDANCE CAMERA
-                          </option>
-                          <option value="RHAZ">
-                            REAR HAZARD AVOIDANCE CAMERA
-                          </option>
-                          <option value="MAST">MAST CAMERA</option>
-                          <option value="CHEMCAM">
-                            CHEMISTRY AND CAMERA COMPLEX
-                          </option>
-                          <option value="NAVCAM">NAVIGATION CAMERA</option>
-                          <option value="MAHLI">MARS HAND LENS IMAGER</option>
-                          <option value="MARDI">MARS DESCENT IMAGER</option>
-                        </select>
-                      </div>
-                    )}
-
-                    {rover === "opportunity" && (
-                      <div>
-                        <select
-                          className="cameraDropdown"
-                          id="opportunityCameras"
-                          value={camera}
-                          onChange={event => {
-                            event.preventDefault();
-                            setCamera(event.target.value);
-                            findMaxSol();
-                          }}
-                          style={cameraDropdownStyles}
-                        >
-                          <option>SELECT A CAMERA</option>
-                          <option value="FHAZ">
-                            FRONT HAZARD AVOIDANCE CAMERA
-                          </option>
-                          <option value="RHAZ">
-                            REAR HAZARD AVOIDANCE CAMERA
-                          </option>
-                          <option value="PANCAM">PANORAMIC CAMERA</option>
-                          <option value="NAVCAM">NAVIGATION CAMERA</option>
-                          <option value="MINITES">MINI-TES</option>
-                        </select>
-                      </div>
-                    )}
-
-                    {rover === "spirit" && (
-                      <div>
-                        <select
-                          required
-                          className="cameraDropdown"
-                          id="spiritCameras"
-                          value={camera}
-                          onChange={event => {
-                            event.preventDefault();
-                            setCamera(event.target.value);
-                            findMaxSol();
-                          }}
-                          style={cameraDropdownStyles}
-                        >
-                          <option>SELECT A CAMERA</option>
-                          <option value="FHAZ">
-                            FRONT HAZARD AVOIDANCE CAMERA
-                          </option>
-                          <option value="FHAZ">
-                            REAR HAZARD AVOIDANCE CAMERA
-                          </option>
-                          <option value="NAVCAM">PANORAMIC CAMERA</option>
-                          <option value="PANCAM">NAVIGATION CAMERA</option>
-                          <option value="MINITES">MINI-TES</option>
-                        </select>
-                      </div>
-                    )}
-
-                    {maxSol !== "" && rover && (
-                      <div className="sol" style={{ marginBottom: "2em" }}>
-                        Enter a number between 0 and {maxSol}:
-                        <br />
-                        <div style={{ fontSize: ".7rem", margin: "1em 0" }}>
-                           Try 55. It returns results for almost all cameras
-                         </div>
-                        <br />
-                        <TextField
-                          type="text"
-                          className="sol-input"
-                          id="sol-input outlined-basic"
-                          variant="outlined"
-                          placeholder={placeholder}
-                          onFocus={event => {
-                            event.preventDefault();
-                            setPlaceholder("");
-                          }}
-                          onBlur={event => {
-                            event.preventDefault();
-                            setPlaceholder("Sol is a Mars-day");
-                          }}
-                          onChange={event => {
-                            event.preventDefault();
-                            setSol(event.target.value);
-                          }}
-                          inputProps={{
-                            style: { textAlign: "center", fontSize: "14px" }
-                          }}
-                        />
-                        <br />
-                      </div>
-                    )}
+                    <RoverDropdown setRover={setRover} setMaxSol={setMaxSol} setCamera={setCamera} rover={rover} />
+                    <CuriosityRover camera={camera} setCamera={setCamera} findMaxSol={findMaxSol} rover={rover}/>
+                    <OpportunityRover camera={camera} setCamera={setCamera} findMaxSol={findMaxSol} rover={rover}/>
+                    <SpiritRover camera={camera} setCamera={setCamera} findMaxSol={findMaxSol} rover={rover} />
+                    <EnterSol maxSol={maxSol} rover={rover} placeholder={placeholder} setPlaceholder={setPlaceholder} setSol={setSol} />
                   </form>
 
                   {marsPhotos.length === 0 && marsError === true && (
-                    <div
-                      className="noPhotos"
-                      style={{
-                        color: "#636363",
-                        fontSize: "14px",
-                        fontFamily: "Space mono"
-                      }}
-                    >
+                    <div className="noPhotos" style={noPhotoStyles}>
                       No data available for that sol date
                     </div>
                   )}
-                  <svg className="searchIcon" style={searchIconStyles} xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
                   <br />
                   {loading === true}
                 </div>
@@ -261,6 +118,7 @@ function MarsRoverPhotos() {
                       <div
                         className="photo-scroll"
                         style={{ color: "#424242" }}
+                        key={index}
                       >
                         <Rover
                           className={`mars${index}`}
