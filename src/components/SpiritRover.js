f1c28b71c8fe315dbda4c4184ef138788d56f3fd
dharmabumzee <luka.belani@gmail.com>
2020-01-30 00:11:58 +0100

    add Select.js component

--- src/components/SpiritRover.js
+++ src/components/SpiritRover.js
@@ -1,36 +1,8 @@
 import React from 'react';
-import { cameraDropdownStyles } from '../styles.js'
+import Select from "./components/Select";
 
 const SpiritRover = ({ camera, setCamera, findMaxSol, rover }) => {
-  return (
-    rover === "spirit" && (
-      <div>
-        <select
-          required
-          className="cameraDropdown"
-          id="spiritCameras"
-          value={camera}
-          onChange={event => {
-            event.preventDefault();
-            setCamera(event.target.value);
-            findMaxSol();
-          }}
-          style={cameraDropdownStyles}
-        >
-          <option>SELECT A CAMERA</option>
-          <option value="FHAZ">
-            FRONT HAZARD AVOIDANCE CAMERA
-          </option>
-          <option value="RHAZ">
-            REAR HAZARD AVOIDANCE CAMERA
-          </option>
-          <option value="NAVCAM">PANORAMIC CAMERA</option>
-          <option value="PANCAM">NAVIGATION CAMERA</option>
-          <option value="MINITES">MINI-TES</option>
-        </select>
-      </div>
-    )
-  );
-}
+  return rover === "spirit" && <Select />;
+};
 
 export default SpiritRover;
