42139941284453ae16af325870e3cd28de59a727
dharmabumzee <luka.belani@gmail.com>
2020-02-26 21:29:32 +0100

    remove bugs

--- src/components/OpportunityRover.js
+++ src/components/OpportunityRover.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Select from "./Select";
 
 const OpportunityRover = ({ camera, setCamera, findMaxSol, rover }) => {
-  return rover === "opportunity" && <Select />;
+  return rover === "opportunity" && <Select camera={camera} setCamera={setCamera} findMaxSol={findMaxSol}/>;
 };
 
 export default OpportunityRover;
