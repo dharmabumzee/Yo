20ecce0fe3c21723a3564a50b5a8f9398d2884dd
dharmabumzee <luka.belani@gmail.com>
2020-02-26 21:28:51 +0100

    remove bugs

--- src/components/SpiritRover.js
+++ src/components/SpiritRover.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Select from "./Select";
 
 const SpiritRover = ({ camera, setCamera, findMaxSol, rover }) => {
-  return rover === "spirit" && <Select />;
+  return rover === "spirit" && <Select camera={camera} setCamera={setCamera} findMaxSol={findMaxSol} />;
 };
 
 export default SpiritRover;
