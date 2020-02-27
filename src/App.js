6bac465d16414aa448a83c602b469c31166f90b7
dharmabumzee <luka.belani@gmail.com>
2020-02-27 10:58:56 +0100

    import DarkModeToggle to App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Homepage from "./Homepage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MarsRoverPhotos from "./components/MarsRoverPhotos";
+import DarkModeToggle from "./components/DarkModeToggle";
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <DarkModeToggle />
       <Route path="/" exact component={Homepage} />
       <Route path="/mars-rover-photos" component={MarsRoverPhotos} />
     </Router>
